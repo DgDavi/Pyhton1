01efad5c8f20ee36ca02cad0898244d0affc7bfb
Davi Gomes <104706788+DgDavi@users.noreply.github.com>
2024-02-26 20:05:39 -0300

    Add files via upload

---
+++ Jokenpo.py
@@ -0,0 +1,53 @@
+#Jokenpo
+
+from random import randint
+from time import sleep
+
+item = ('Pedra', 'Papel', 'Tesoura') 
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Qual a sua jogada? '))
+print('JO')
+sleep(0.5)
+print('KEN')
+sleep(0.5)
+print('PO!!!')
+sleep(0.5)
+
+print('-=' * 11)
+print(f'O computador jogou {(item[computador])}.')
+print(f'O jogador jogou {(item[jogador])}.')
+print('-=' * 11)
+
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVALIDA')
+
+if computador == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADA INVALIDA')
+
+if computador == 2:
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVALIDA')
