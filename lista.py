4c437940f193c90bf8705c8342b98e0acaab7991
Davi Gomes <104706788+DgDavi@users.noreply.github.com>
2024-03-06 17:22:50 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,33 @@
+import os
+lista = []
+
+while True:
+    print('Selecione uma opção')
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    if opcao == 'i':
+        os.system('cls')
+        valor = input('Valor: ')
+        lista.append(valor)
+
+    elif opcao == 'a':
+        try:
+            os.system('cls')
+            indice = int(input('Qual o indice que deseja apagar? '))
+            del lista[indice]
+        except ValueError:
+            print('Digite apenas números inteiros.')
+
+        except IndexError:
+            print('Este índice não existe na lista.')
+
+    elif opcao == 'l':
+        os.system('cls')
+        if len(lista) == 0:
+            print('Não há nada para listar.')
+
+        for i, valor in enumerate(lista):
+            print(i, valor)
+
+    else:
+        print('Escolha i, a ou l.')
