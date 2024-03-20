c4c5ab4481e5ff1f0ddc1ad9296655ed46f91ca1
Davi Gomes <104706788+DgDavi@users.noreply.github.com>
2024-03-20 09:49:23 -0300

    Update calculadora_funcao.py

--- calculadora_funcao.py
+++ calculadora_funcao.py
@@ -1,3 +1,4 @@
+# Calculadora Utilizando Funções
 def soma(a, b):
     print(a + b)
     
@@ -9,20 +10,40 @@ def multiplicacao(a, b):
     
 def divisao(a, b):
     print(a / b)
-    
-num1 = float(input('Digite o primeiro número:  '))
-num2 = float(input('Digite o segundo número: '))
-print('Qual operação você deseja? ')
-operacao = int(input('[1]Soma, [2]Subtração, [3]Multiplicação, [4]Divisão: '))
 
-if operacao == 1:
-    soma(num1, num2)
-    
-elif operacao == 2:
-    subtracao(num1, num2)
-    
-elif operacao == 3:
-    multiplicacao(num1, num2)
-    
-elif operacao == 4:
-    divisao(num1, num2)
+while True:
+    try:
+        num1 = float(input('Digite o primeiro número:  '))
+        num2 = float(input('Digite o segundo número: '))
+        print('Qual operação você deseja? ')
+        operacao = int(input('[1]Soma, [2]Subtração, [3]Multiplicação, [4]Divisão: '))
+    except ValueError:
+        print('Você não digitou algo válido.')
+        continue
+
+    if operacao >= 5 or operacao <= 0:
+        print('Você não escolheu uma operação válida')
+        continue
+
+    if operacao == 1:
+        soma(num1, num2)
+        
+    elif operacao == 2:
+        subtracao(num1, num2)
+        
+    elif operacao == 3:
+        multiplicacao(num1, num2)
+        
+    elif operacao == 4:
+        divisao(num1, num2)
+
+    print('Você deseja sair?')
+    saida = input('[s]im [n]ão')
+
+    if saida == 's':
+        break
+    elif saida =='n':
+        break
+    else:
+        print('Você não digitou algo válido')
+        continue
