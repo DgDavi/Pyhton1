06508259d3e22465b924697d9a796183caf61d8a
Davi Gomes <104706788+DgDavi@users.noreply.github.com>
2024-05-13 13:24:23 -0300

    Update Jokenpo.py

--- Jokenpo.py
+++ Jokenpo.py
@@ -1,88 +1,4 @@
-# Jokenpo
-
-from os import system
-from random import randint
-from time import sleep
-
-while True:
-    item = ('Pedra', 'Papel', 'Tesoura') 
-    computador = randint(0, 2)
-    try:
-        print('''Suas opções:
-[ 0 ] Pedra
-[ 1 ] Papel
-[ 2 ] Tesoura''')
-        jogador = int(input('Qual a sua jogada? '))
-    except ValueError:
-        system('cls')
-        print('Digite apenas os números abaixo referente as jogadas.')
-        continue
-     
-    if jogador >= 3 or jogador < 0:
-        system('cls')
-        print('Digite apenas os números abaixo referente as jogadas.')
-        continue
-        	
-    print('JO')
-    sleep(0.5)
-    print('KEN')
-    sleep(0.5)
-    print('PO!!!')
-    sleep(0.5)
-
-    print('-=' * 11)
-    print(f'O computador jogou {(item[computador])}.')
-    print(f'O jogador jogou {(item[jogador])}.')
-    print('-=' * 11)
-
-    if computador == 0:
-        if jogador == 0:
-                    print('EMPATE')
-        elif jogador == 1:
-                    print('JOGADOR VENCE')
-        elif jogador == 2:
-                    print('COMPUTADOR VENCE')
-        else:
-                    print('JOGADA INVALIDA')
-
-    if computador == 1:
-        if jogador == 0:
-                print('COMPUTADOR VENCE')
-        elif jogador == 1:
-                print('EMPATE')
-        elif jogador == 2:
-                print('JOGADOR VENCE')
-        else:
-                print('JOGADA INVALIDA')
-        
-    if computador == 2:
-        if jogador == 0:
-                print('JOGADOR VENCE')
-        elif jogador == 1:
-                print('COMPUTADOR VENCE')
-        elif jogador == 2:
-                print('EMPATE')
-        else:
-                print('JOGADA INVALIDA')
-                
-    print('Você quer parar?')
-    saida = input('[s]im [n]ão: ')
-    
-    if saida == 's':
-        system('cls')
-        break
-    
-    elif saida == 'n':
-        continue
-    
-    else:
-        system('cls')
-        print('Você não digitou algo válido, o jogo continuará.')
-        continue
-
-
-
-"""
+#Jokenpo
 from random import choice
 from time import sleep 
 
@@ -95,9 +11,10 @@ while True:
 [ 2 ] Papel
 [ 3 ] Tesoura""")
     try:
-        jogador = float(input('Digite o Número de acordo com a sua jogada:'))
+        jogador = float(input('Digite o número de acordo com a sua jogada:'))
     except ValueError:
         print('Você não digitou algo válido. Tente novamente.')
+        sleep(0.8)
         continue
 
     print('JO')
@@ -133,7 +50,19 @@ while True:
         if sorteio == 'pedra':
             print('VOCÊ PERDEU!')
         elif sorteio == 'papel':
-            print('VOÊ GANHOU')
+            print('VOCÊ GANHOU')
         elif sorteio == 'tesoura':
             print('EMPATE!')
-"""
+
+    print('Você deseja sair?')
+    saida = input('[s]im [n]ão')
+    
+    if saida == 's':
+        print('Fim de Jogo!')
+        break
+    elif saida == 'n':
+        continue
+    else:
+        print('Você não digitou algo válido. O jogo contiuará.')
+        sleep(0.8)
+        continue
